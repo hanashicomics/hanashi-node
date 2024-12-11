425f28b2ed3159b4169d90fab44a525872ce611a
EthanLeRoux <ethanleroux119@gmail.com>
2024-12-11 21:45:35 +0200

    Add host and port

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const host = '0.0.0.0';
+
+app.listen(PORT,host, () => {
     console.log(`Server started on port ${PORT}`);
 })
 
